2044bc6d23b108924d6deb536f96f7aebdbdbf79
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2024-06-04 06:16:33 -0400

    Adding support for `pip`

--- setup.py
+++ setup.py
@@ -1,32 +1,16 @@
 # Strictly for use outside of the WebUI. This file will allow you to install Unprompted via pip. Example:
 # pip install unprompted@git+https://github.com/ThereforeGames/unprompted
 
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-import os
-import shutil
-
-
-class CustomInstall(install):
-	def run(self):
-		# Run the standard install
-		install.run(self)
-
-		# Move the files and directories into the 'unprompted' directory
-		installation_path = os.path.join(self.install_lib, "unprompted")
-		os.makedirs(installation_path, exist_ok=True)
-		for item in ["shortcodes", "lib_unprompted", "templates", "config.json"]:
-			shutil.move(os.path.join(self.install_lib, item), os.path.join(installation_path, item))
-
+from setuptools import setup
 
 setup(
-    name="Unprompted",
-    version="11.0.2",
-    packages=find_packages(),
+    name='Unprompted',
+    version='11.0.2',
+    package_dir={'unprompted': '.'},
+    packages=['unprompted.lib_unprompted', 'unprompted.shortcodes', 'unprompted.templates'],
     package_data={
-        '': ['config.json', 'shortcodes/*', 'lib_unprompted/*', 'templates/common/*'],
-    },
-    cmdclass={
-        'install': CustomInstall,
+        'unprompted': ['config.json'],
+        'unprompted.shortcodes': ['basic/*.txt', 'stable_diffusion/*.txt'],
+        'unprompted.templates': ['common/*.txt']
     },
 )

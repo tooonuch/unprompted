aef63a26f81b46c33f681b34832a6ca2e09a57fc
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2024-06-04 05:01:22 -0400

    Support for `pip`

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Strictly for use outside of the WebUI. This file will allow you to install Unprompted via pip. Example:
+# pip install unprompted@ git+https://github.com/ThereforeGames/unprompted
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Unprompted",
+    version="11.0.2",
+    packages=find_packages(),
+)
